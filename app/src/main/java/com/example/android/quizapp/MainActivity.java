184e5002ef6de643150f07ec25db9c0360db4668
cvikvi <willjrckt@yahoo.com>
2018-03-16 09:24:48 -0500

    Adding A Couple of Questions

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     *
+     *
+     *
+     */
+
+    private void scoreQuiz() {
+
+
+        CheckBox questionOne = (CheckBox) findViewById(R.id.question_1_answer);
+
+
+    }
+
+
+
+
+
 }
